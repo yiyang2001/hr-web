7f13ec9599482188341d5ae33e0085082d792a12
yiyang2001 <yiyang1357@gmail.com>
2022-09-22 17:02:03 +0800

    Update settings.py

--- HRMSPROJECT/settings.py
+++ HRMSPROJECT/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import mimetypes
 # import django_heroku
 # import dj_database_url
 
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
